f104978fec0f41c162f232028a3cb07d79fc7666
Muzafar-Ali <84478313+Muzafar-Ali@users.noreply.github.com>
2026-05-13 04:08:24 +0500

    feat:(user-service): implement Redis-based session authentication middleware

--- user-service/src/modules/auth/auth.controllers.ts
+++ user-service/src/modules/auth/auth.controllers.ts
@@ -10,48 +10,6 @@ import { createWebSession, setWebSessionCookie } from '../../utils/sessionHelper
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  // loginHandler = async (req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction) => {
-  //   try {
-  //     const safeData = userLoginSchema.safeParse(req.body)
-
-  //     if(!safeData.success) {
-  //       throw new ApiErrorHandler(400, formatZodError(safeData.error))
-  //     }
-
-  //     const dto: UserLoginDto = safeData.data;
-  //     const identifier = (dto.email ?? dto.username)!;
-
-  //     const loginContext: TLoginContext = {
-  //       identifier,
-  //       ip: req.ip,
-  //       userAgent: req.headers["user-agent"] ?? "",
-  //     }
-
-  //     const user = await this.authService.userLogin(dto, loginContext);
-
-  //     const payload = {
-  //       userId: user.id,
-  //       email: user.email,
-  //       username: user.username
-  //     }
-
-  //     generateJwtTokenAndSaveCookies(payload, res)
-
-  //     res.status(200).json({
-  //       success: true,
-  //       message: "logged in successfully",
-  //       data: {
-  //         userId: user.id,
-  //         username: user.username,
-  //         email: user.email,
-  //       }
-  //     })
-
-  //   } catch (error) {
-  //     next(error)
-  //   }
-  // }
-
   webLoginHandler = async (
     req: Request<Record<string, never>, any, UserLoginDto>,
     res: Response,
--- user-service/src/modules/user/user.routes.ts
+++ user-service/src/modules/user/user.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { UserController } from './user.controllers.js';
-import isAuthenticated from '../../middlewares/isAuthenticated.js';
+import isAuthenticatedRedis from '../../middlewares/isAuthenticatedRedis.js';
 
 const userRoutes = (userController: UserController) => {
   const router = Router();
 
   router.route('/').post(userController.createUser);
-  router.route('/profile/me').patch(isAuthenticated, userController.updateMyProfile);
-  router.route('/profile/profile-image').patch(isAuthenticated, userController.updateProfileImage);
+  router.route('/profile/me').patch(isAuthenticatedRedis, userController.updateMyProfile);
+  router.route('/profile/profile-image').patch(isAuthenticatedRedis, userController.updateProfileImage);
   router.route('/profile/id/:id').get(userController.getProfileById);
   router.route('/profile/username/:username').get(userController.getProfileByUsername);
 
